a654dd51deae6285fa222d9f153bf15fd60467ce
dato <dato.tchkadua.1@iliauni.edu.ge>
2022-05-31 22:17:25 +0400

    Updates

---
+++ precache-manifest.044fd108cdc5e871c2db55ddefb3a96c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8f4b84769c65d5ffbcfb6720ef5d0f",
+    "url": "/Reviews-react-course-/index.html"
+  },
+  {
+    "revision": "e15c5aca17506cdde833",
+    "url": "/Reviews-react-course-/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/Reviews-react-course-/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reviews-react-course-/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e15c5aca17506cdde833",
+    "url": "/Reviews-react-course-/static/js/main.c29f012d.chunk.js"
+  },
+  {
+    "revision": "ffdb49f9d6f80cffb887",
+    "url": "/Reviews-react-course-/static/js/runtime-main.b8e51419.js"
+  }
+]);
